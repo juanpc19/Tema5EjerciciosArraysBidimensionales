2423528550ddb10c6c18e75250a6489cbb3dff9f
juanpc19 <juan.perez@iesnervion.es>
2023-01-24 12:34:36 +0100

    ej 3 y 4 comentados

--- Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio3.java
+++ Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio3.java
@@ -9,75 +9,90 @@ public class Ejercicio3 {
 
 		int tabla[][] = new int[4][5];
 
-		int alumno = 1;
-		
-		int notaMinima=10;
-		
-		int notaMaxima=0;
-		
-		int notaMedia=0;
+		int notaMinima = 10;// Nota minima inicializada a 10 que es la maxima posible
+
+		int notaMaxima = 0;// nota maxima inicializada a 0 que es la minima posible
+
+		int notaMedia = 0;// nota medai inicializada a 0 para usar en bucle
 
 		Scanner dogma = new Scanner(System.in);
 
 		// for para recorrer eje y de array tabla
 		for (int i = 0; i < tabla.length; i++) {
-			System.out.println("Introduzca las notas del Alumno" + (alumno) + ": ");
+			// mensaje por pantalla personalizado con valor de posicion actual de eje y +1
+			System.out.println("Introduzca las notas del Alumno " + (i + 1) + ": ");
 
 			// bucle for para recorrer eje x de array tabla
-			//tabla[0].length indica longitud de eje x en posicion 0 de eje y
-			//entre los corchetes puede ir cualquier valor solo ha de estar relleno
-			//para que calcule la longitud del eje x y no del y
+			// tabla[0].length indica longitud de eje x en posicion 0 de eje y
+			// entre los corchetes puede ir cualquier valor solo ha de estar relleno
+			// para que calcule la longitud del eje x y no del y
 			for (int j = 0; j < tabla[0].length; j++) {
 
 				// doy a array tabla posicion eje y = i eje x = j valor igual a
-				// la suma de sus posiciones bidimensionales (eje y, eje x)
-				// multiplicadas por 10
-				tabla[i][j] = dogma.nextInt();	
+				// siguienteentrada de datos por teclado
+				tabla[i][j] = dogma.nextInt();
 			}
-			alumno++;
 		}
 
 		dogma.close();
 
+		// for para recorrer eje y de array tabla
 		for (int i = 0; i < tabla.length; i++) {
 
 			// hago println de array tabla en posicion de eje y con valor i
 			// lo cual hara print de el eje x con todas sus posiciones en cada linea
+			// mensaje por pantalla personalizado con valor de alumno igual a posicion
+			// actual de eje y +1
+			System.out.print("Las notas del Alumno " + (i + 1) + " son: ");
 			System.out.println(Arrays.toString(tabla[i]));
 		}
 
 		// for para recorrer eje y de array tabla
 		for (int i = 0; i < tabla.length; i++) {
-			
-			
 
 			// bucle for para recorrer eje x de array tabla
-			//tabla[0].length indica longitud de eje x en posicion 0 de eje y
-			//entre los corchetes puede ir cualquier valor solo ha de estar relleno
-			//para que calcule la longitud del eje x y no del y
+			// tabla[0].length indica longitud de eje x en posicion 0 de eje y
+			// entre los corchetes puede ir cualquier valor solo ha de estar relleno
+			// para que calcule la longitud del eje x y no del y
 			for (int j = 0; j < tabla[0].length; j++) {
 
-								
-				if (tabla[i][j]<notaMinima) {
-					
+				// compruebo si el valor del elemento de posicion j en fila i es menor a
+				// notaMinima
+				if (tabla[i][j] < notaMinima) {
+					// de serlo doy notaMinima el valor de dicho elemento
+					notaMinima = tabla[i][j];
+
+					// compruebo si el valor del elemento de posicion j en fila i es menor a
+					// notaMaxima
+				} else if (tabla[i][j] > notaMaxima) {
+					// de serlo doy notaMaxima el valor de dicho elemento
+					notaMaxima = tabla[i][j];
 				}
-				notaMinima=tabla[i][j];
+
+				// almaceno en noMedia el valor del elemento contenido en eje y i eje x j
+				notaMedia += tabla[i][j];
 			}
-			
-			System.out.println("La nota minima del Alumno" + (alumno) + "es: ");
-			System.out.println("La nota maxima del Alumno" + (alumno) + "es: ");
-			System.out.println("La nota media del Alumno" + (alumno) + "es: ");
-			
-			alumno++;
-		
-			notaMinima=10;
-			notaMaxima=0;
-			notaMedia=0;
-			
+
+			// doy a notaMedia valor igual a todas las notas guardadas con anterioridad
+			// dividido entre longitud de eje x y le aplico casting a tipo int
+			notaMedia = (int) (notaMedia / tabla[0].length);
+
+			// hago multiples prints mostrando notaMinima, notaMaxima y notaMedia
+			// mensaje por pantalla personalizado con valor de alumno igual a posicion
+			// actual de eje y +1
+			System.out.println();
+			System.out.println("La nota minima del Alumno " + (i + 1) + " es: " + notaMinima);
+			System.out.println("La nota maxima del Alumno " + (i + 1) + " es: " + notaMaxima);
+			System.out.println("La nota media del Alumno " + (i + 1) + " es: " + notaMedia);
+			System.out.println();
+
+			// reinicio los valores de notaMinima, notaMaxima y notaMedia a sus valores
+			// antes del bucle para poder usarlos correctamente en la siguiente iteracion
+			notaMinima = 10;
+			notaMaxima = 0;
+			notaMedia = 0;
 		}
 
-		
-		
 	}
 
 }
--- Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio4.java
+++ Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio4.java
@@ -1,9 +1,36 @@
 package ejercicio;
 
+import java.util.Arrays;
+
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int num[][] = new int[3][6];
+
+		// asigno valor a elementos dentro del array bidimensional de forma manual
+		num[0][0] = 0;
+
+		num[0][1] = 30;
+
+		num[0][2] = 2;
+
+		num[0][5] = 5;
+
+		num[1][0] = 75;
+
+		num[1][4] = 0;
+
+		num[2][2] = -2;
+
+		num[2][3] = 9;
+
+		num[2][5] = 11;
+
+		// bucle for para recorrer las filas y hacer print de cada una de ellas
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(Arrays.toString(num[i]));
+		}
 
 	}
 

384ba98c9af1f0fd1d076bc3aa17f6c5a75a71f8
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-01-30 20:12:18 +0100

    no funka pero deberia?

--- Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio8.java
+++ Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio8.java
@@ -8,6 +8,9 @@ public class Ejercicio8 {
 
 		int matriz[][] = new int[5][5];
 
+		boolean simetrica = false;
+
+		// genera matriz
 		for (int fila = 0; fila < matriz.length; fila++) {
 
 			for (int columna = 0; columna < matriz[fila].length; columna++) {
@@ -18,13 +21,31 @@ public class Ejercicio8 {
 
 		}
 
+		// print amatriz
 		for (int fila = 0; fila < matriz.length; fila++) {
 
 			System.out.println(Arrays.toString(matriz[fila]));
 		}
 
-		// mirar 7 y imagen matriz simetrica, crear boleana para evañluear mensaje al
-		// final de comprobar todas las posiciones
+		// recorre array comprobando si es simetrico partiendo de que no lo es
+		for (int fila = 0; fila < matriz.length; fila++) {
+
+			for (int columna = 0; columna < matriz[fila].length; columna++) {
+
+				// si se da cambia a que si es simetrica
+				if (matriz[fila][columna] == matriz[columna][fila]) {
+					simetrica = true;
+					// sino permanece false
+				} else {
+					simetrica = false;
+				}
+
+			}
+
+		}
+
+		// ternario evalua estado simetrica y devuelve respuesta
+		System.out.println(simetrica ? "Es simetrica." : "No es simetrica.");
 
 	}
 

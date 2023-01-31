1ee8b8724065c9c6580e09f954d1e4688463ba75
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-01-31 19:19:01 +0100

    comentarios ej 7 y 8

--- Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio7.java
+++ Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio7.java
@@ -11,10 +11,13 @@ public class Ejercicio7 {
 
 		int matrizB[][] = new int[4][4];
 
-		int auxiliar = 0;
+		int auxiliar = 0;// variable donde guardare
 
 		Scanner dogma = new Scanner(System.in);
 
+		// bucle para recorrer array matrizA en horizontal ([fila][columna]) dandole
+		// valor a sus elementos
+		// igual al introducido por teclado
 		for (int fila = 0; fila < matrizA.length; fila++) {
 
 			for (int columna = 0; columna < matrizA[0].length; columna++) {
@@ -22,41 +25,60 @@ public class Ejercicio7 {
 				System.out.println("Introduzca valor para el siguiente elemento: ");
 				matrizA[fila][columna] = dogma.nextInt();
 			}
+
 		}
 
 		dogma.close();
 
+		// print de array
 		for (int fila = 0; fila < matrizA.length; fila++) {
 			System.out.println(Arrays.toString(matrizA[fila]));
 		}
 
+		// print para espacio
 		System.out.println();
+
+		// bucle para recorrer array matrizB en vertical ([columna][fila])
+		// lo usare para transponer la matrizA y copiarla en la matrizB
 		for (int columna = 0; columna < matrizA.length; columna++) {
 
 			for (int fila = 0; fila < matrizA.length; fila++) {
 
+				// dandole valor a sus elementos igual a el valor almacenado en matrizA con
+				// sus posiciones siendo invertidas a las de matrizB (matrizA[fila][columna])
 				matrizB[columna][fila] = matrizA[fila][columna];
 
 			}
+			// print de fila una vez asignado valor a sus elementos
 			System.out.println(Arrays.toString(matrizB[columna]));
 		}
 
+		// print para espacio
 		System.out.println();
 
+		// bucle para recorrer matrizA en vertical ([columna][fila])
+		// que usare para transponer la matrizA sin usar otro array (matrizB)
 		for (int columna = 0; columna < matrizA.length; columna++) {
 
 			for (int fila = columna; fila < matrizA[0].length; fila++) {
 
+				// guardo en auxiliar valor del elemento en la posicion actual
+				// para no perderlo al macharlo en la siguiente linea de codigo
 				auxiliar = matrizA[fila][columna];
 
+				// doy a elemento en posicion [fila][columna] valor de elemento en posicion
+				// [columna][fila]
 				matrizA[fila][columna] = matrizA[columna][fila];
 
+				// doy a elemento en posicion [columna][fila] valor previamente guardado en
+				// auxiliar
 				matrizA[columna][fila] = auxiliar;
 
 			}
 
 		}
 
+		// print de array matrizA
 		for (int fila = 0; fila < matrizA.length; fila++) {
 			System.out.println(Arrays.toString(matrizA[fila]));
 		}
--- Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio8.java
+++ Tema5EjerciciosArraysBidimensionales/src/ejercicio/Ejercicio8.java
@@ -11,47 +11,45 @@ public class Ejercicio8 {
 
 		boolean simetrica = false;
 
-		
-		Scanner dogma=new Scanner (System.in);
-		
-		// genera matriz aleatoria
-		/*for (int fila = 0; fila < matriz.length; fila++) {
-
-			for (int columna = 0; columna < matriz[fila].length; columna++) {
-
-				matriz[fila][columna] = (int) (Math.random() * 10);
-
-			}
-
-		}
-
-		*/
-		//meter a mano una matriz simetrica para comprobar funcionamiento
+		// bucle for para generar matriz con valores aleatoris del 0 al 10
 		for (int fila = 0; fila < matriz.length; fila++) {
 
 			for (int columna = 0; columna < matriz[fila].length; columna++) {
-				System.out.println("introduzca numero");
-				matriz[fila][columna] = dogma.nextInt();
+
+				matriz[fila][columna] = (int) (Math.random() * 11);
 
 			}
-			System.out.println("siguiente linea");
+
 		}
-		
-		// print amatriz
+
+		// genera matriz a mano para comprobacion
+		/*
+		 * Scanner dogma=new Scanner (System.in); for (int fila = 0; fila <
+		 * matriz.length; fila++) {
+		 * 
+		 * for (int columna = 0; columna < matriz[fila].length; columna++) {
+		 * System.out.println("introduzca numero"); matriz[fila][columna] =
+		 * dogma.nextInt();
+		 * 
+		 * } System.out.println("siguiente linea"); } dogma.close();
+		 */
+
+		// print de matriz
 		for (int fila = 0; fila < matriz.length; fila++) {
 
 			System.out.println(Arrays.toString(matriz[fila]));
 		}
 
-		// recorre array comprobando si es simetrico partiendo de que no lo es
+		// recorre array comprobando si la matriz es simetrica o no partiendo de que no
+		// lo es y cambiando boleana simetrica en caso de que lo sea o no
 		for (int fila = 0; fila < matriz.length; fila++) {
 
 			for (int columna = 0; columna < matriz[fila].length; columna++) {
 
-				// si se da cambia a que si es simetrica
+				// comprobacion de elemento de una celda igual al de su "opuesta"
 				if (matriz[fila][columna] == matriz[columna][fila]) {
 					simetrica = true;
-					// sino permanece false
+					// de no ser iguales
 				} else {
 					simetrica = false;
 					break;
@@ -62,7 +60,7 @@ public class Ejercicio8 {
 
 		}
 
-		// ternario evalua estado simetrica y devuelve respuesta
+		// ternario evalua estado simetrica y devuelve respuesta acorde
 		System.out.println(simetrica ? "Es simetrica." : "No es simetrica.");
 
 	}
